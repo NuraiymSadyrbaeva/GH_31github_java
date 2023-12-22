193f7f8a1a1c8f7261af974fed48692994c3162d
Nuraiym Sadyrbaeva <120064238+NuraiymSadyrbaeva@users.noreply.github.com>
2023-12-22 23:16:23 +0000

    elka2

--- elka.py
+++ elka.py
@@ -1,9 +1,16 @@
-def print_pyramid_tree(height):
-    for i in range(height):
-        print(" " * (height - i - 1) + "*" * (2 * i + 1))
+# Funktion zum Drucken eines Baumzweigs
+def print_branch(branch_length, spaces):
+    if branch_length > 5:
+        print(" " * spaces + "*" * branch_length)
+        print_branch(branch_length - 2, spaces + 1)
+        print_branch(branch_length - 2, spaces - 1)
 
-# Höhe der Pyramide festlegen
-tree_height = 7
+# Funktion zum Drucken des Weihnachtsbaums
+def print_tree(height):
+    print_branch(height * 2, height)
 
-# Weihnachtsbaum in Pyramidenform ausgeben
-print_pyramid_tree(tree_height)
+# Höhe des Baumes festlegen
+tree_height = 5
+
+# Baum in Textform ausgeben
+print_tree(tree_height)
